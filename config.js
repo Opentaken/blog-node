24143580b6dab1390a54d0180de72dfdf50b57d2
recall <13428204829@163.com>
2018-04-25 15:01:35 +0800

    修改注释

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  isRedis: false,//是否使用ｒｅｄｉｓ保存ｓｅｓｓｉｏｎ
+  isRedis: false,//是否使用redis保存session
   RedisOptions: {
     host: '127.0.0.1', //redis数据库地址
     port: '6379', //redis数据库端口
