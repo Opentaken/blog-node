38744f071835b122dded649ac4c5a0c62622c0fd
recall <13428204829@163.com>
2018-05-04 14:24:29 +0800

    添加分号

--- controls/userControl.js
+++ controls/userControl.js
@@ -18,34 +18,34 @@ var UserControl = {
         //校验参数
         try{
             if (!(name.length >= 1 && name.length <= 10)) {
-                throw new Error('名字请限制在 1-10 个字符')
+                throw new Error('名字请限制在 1-10 个字符');
             }
             if (['m', 'f', 'x'].indexOf(gender) === -1) {
-                throw new Error('性别只能是 m、f 或 x')
+                throw new Error('性别只能是 m、f 或 x');
             }
             if (!(bio.length >= 1 && bio.length <= 30)) {
-                throw new Error('个人简介请限制在 1-30 个字符')
+                throw new Error('个人简介请限制在 1-30 个字符');
             }
             if (!req.files.avatar.name) {
-                throw new Error('缺少头像')
+                throw new Error('缺少头像');
             }
             if (password.length < 6) {
-                throw new Error('密码至少 6 个字符')
+                throw new Error('密码至少 6 个字符');
             }
             if (password !== repassword) {
-                throw new Error('两次输入密码不一致')
+                throw new Error('两次输入密码不一致');
             }
         }catch (e){
-            console.log("-------")
-            console.log(e.message)
-            console.log("-------")
+            console.log("-------");
+            console.log(e.message);
+            console.log("-------");
             // 注册失败，异步删除上传的头像
             if(req.files.avatar.name){
                 fs.unlink(req.files.avatar.path);
             }
             req.flash('error', e.message);
             res.redirect(303,'/users/signup');
-            return
+            return;
         }
 
         //生成salt的迭代次数
